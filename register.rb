8d0176bb89eba874a3c481b746aeb296c7af5dd4
drutz-cpnv <dimitri.rutz@eduvaud.ch>
2024-05-15 15:53:35 +0200

    exercise implementation

---
+++ register.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+class CashDesk
+
+
+end
+
+
+
+class PaymentMethod
+
+  class CashPayment < self
+  end
+
+  class CardPayment < self
+  end
+
+  class CardPayment < self
+  end
+
+
+end
+
+class PaymentTerminal
+
+  AVAILABLE_METHODS = [PaymentMethod::CashPayment]
+
+end
